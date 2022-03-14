0bce86b2a599ea47f0c7639859ac45ad05077a1e
PotatoNutLover <96751283+PotatoNutLover@users.noreply.github.com>
2022-03-14 18:50:52 +0300

    ASCIIConverterApp

---
+++ ASCIIConverter/Controller.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using PotatoUtils.ASCIIImageConverter;
+using System.Windows.Media.Imaging;
+using ImageConverter = PotatoUtils.ASCIIImageConverter.ImageConverter;
+using System.Threading.Tasks;
+
+namespace ASCIIConverter
+{
+    public class Controller
+    {
+        public string InputPath = " ";
+        public string OutputPath = "Images/image.txt";
+        public int CompressionLevel { get; private set; } = 1;
+        public int BrightnessLevel { get; private set; } = 0;
+
+        private BitmapImage _displayedImage;
+        private bool _displayedImageInit = false;
+
+        public async Task ConvertAsync()
+        {
+            await Task.Run(() => Convert());
+        }
+
+        public void Convert()
+        {
+            ImageConverterSettings settings = new ImageConverterSettings(InputPath)
+            {
+                OutputPath = OutputPath,
+                CompressionLevel = CompressionLevel,
+                BrightnessLevel = (float)BrightnessLevel / 100
+            };
+
+            ImageConverter converter = new ImageConverter();
+            converter.Convert(settings);
+        }
+
+        public bool TryLoadBitmapImage(string imagePath,out BitmapImage bitmapImage)
+        {
+            try
+            {
+                _displayedImage = new BitmapImage();
+                _displayedImage.BeginInit();
+                _displayedImage.UriSource = new Uri(imagePath);
+                _displayedImage.EndInit();
+                _displayedImageInit = true;
+                bitmapImage = _displayedImage;
+                return true;
+            }
+            catch
+            {
+                bitmapImage = new BitmapImage();
+
+                return false;
+            }
+           
+            
+        }
+
+        public void SetCompressionLevel(string compressionLevel)
+        {
+            Int32.TryParse(compressionLevel, out int result);
+            if (result < 1)
+                result = 1;
+            CompressionLevel = result;
+        }
+
+        public void SetBrightnessLevel(double brightnessLevel)
+        {
+            BrightnessLevel = Math.Clamp((int)brightnessLevel, -100, 100);
+        }
+
+        public string GetResolutionString()
+        {
+            if (_displayedImageInit)
+                return $"{_displayedImage.PixelWidth}x{_displayedImage.PixelHeight} ({_displayedImage.PixelWidth / CompressionLevel}x{_displayedImage.PixelHeight / CompressionLevel})";
+            else
+                return "";
+        }
+    }
+}
---
+++ ASCIIConverter/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ASCIIConverter
+{
+    public partial class MainWindow : Window
+    {
+        private Controller controller = new Controller();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBoxCompressionLevel.Text = controller.CompressionLevel.ToString();
+        }
+
+        private async void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxCompressionLevel.Text = controller.CompressionLevel.ToString();
+
+            ConvertingStateLabel.Visibility = Visibility.Visible;
+            this.IsEnabled = false;
+
+            await controller.ConvertAsync();
+
+            this.IsEnabled = true;
+            ConvertingStateLabel.Visibility = Visibility.Hidden;
+        }
+
+
+        private void ChooseFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            if(Directory.Exists(controller.OutputPath.Split('/')[0]))
+                fileDialog.InitialDirectory = controller.OutputPath.Split('/')[0];
+            fileDialog.AddExtension = true;
+            fileDialog.DefaultExt = ".txt";
+            if (fileDialog.ShowDialog() == true)
+            {
+                controller.OutputPath = fileDialog.FileName;
+                LabelPath.Content = fileDialog.FileName;
+            }      
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ?
+                DragDropEffects.All : DragDropEffects.None;
+        }
+
+        private void Window_DragOver(object sender, DragEventArgs e)
+        {
+            BitmapImage displayedImage;
+
+            if (controller.TryLoadBitmapImage(((string[])e.Data.GetData(DataFormats.FileDrop))[0], out displayedImage) == true)
+            {
+                HelpLabel.Visibility = Visibility.Hidden;
+                controller.InputPath = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+                PickedImage.Stretch = Stretch.Fill;
+                PickedImage.Source = displayedImage;
+                ResolutionLabel.Content = controller.GetResolutionString();
+            }
+            else
+                MessageBox.Show("Bad Image");
+        }
+
+        private void TextBoxCompressionLevel_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TextBoxCompressionLevel.Text != "")
+            {
+                controller.SetCompressionLevel(TextBoxCompressionLevel.Text);
+                TextBoxCompressionLevel.Text = controller.CompressionLevel.ToString();
+                if(ResolutionLabel != null)
+                    ResolutionLabel.Content = controller.GetResolutionString();
+            }
+        }
+
+        private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            controller.SetBrightnessLevel(BrightnessSlider.Value);
+            BrighnessLabel.Content = controller.BrightnessLevel;
+        }
+
+
+    }
+}
